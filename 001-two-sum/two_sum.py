c95a88dcf5c9818838823e80e74212ba4a21db6a
luocj <cnluocj@gmail.com>
2017-04-28 16:13:06 +0800

    finish - 001-two-sum 参考了答案，自己写的超时了

---
+++ 001-two-sum/two_sum.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        see the answer
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if len(nums) < 2:
+            return False
+
+        map = {}
+        for index, num in enumerate(nums):
+            if num not in map:
+                map[target-num] = index
+            else:
+                return [map[num], index]
+
+print Solution().twoSum([2, 7, 11, 15], target=9)
+    

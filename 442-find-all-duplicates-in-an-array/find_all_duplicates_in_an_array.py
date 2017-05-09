7979ad337ab443e4b49ddbafdf9c82b71aef0e1d
luocj <cnluocj@gmail.com>
2017-05-09 17:30:09 +0800

    优化 - 442

--- 442-find-all-duplicates-in-an-array/find_all_duplicates_in_an_array.py
+++ 442-find-all-duplicates-in-an-array/find_all_duplicates_in_an_array.py
@@ -1,21 +1,35 @@
 """
 41.25%
+87.54%
 """
 
 class Solution(object):
+    # def findDuplicates(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: List[int]
+    #     """
+
+    #     mp = {}
+    #     result = []
+    #     for n in nums:
+    #         try:
+    #             mp[n] += 1
+    #         except:
+    #             mp[n] = 1
+    #         else:
+    #             result.append(n)
+    #     return result
+
     def findDuplicates(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
         """
-
-        mp = {}
         result = []
         for n in nums:
-            try:
-                mp[n] += 1
-            except:
-                mp[n] = 1
+            if nums[abs(n)-1] < 0:
+                result.append(abs(n))
             else:
-                result.append(n)
+                nums[abs(n)-1] *= -1
         return result

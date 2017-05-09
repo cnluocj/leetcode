ce1fd12ebfbbc2302564921caf5da8945ddd95ef
luocj <cnluocj@gmail.com>
2017-05-09 17:31:06 +0800

    no message

--- 442-find-all-duplicates-in-an-array/find_all_duplicates_in_an_array.py
+++ 442-find-all-duplicates-in-an-array/find_all_duplicates_in_an_array.py
@@ -23,6 +23,12 @@ class Solution(object):
 
     def findDuplicates(self, nums):
         """
+        O(1) space not including the input and output variables
+
+The idea is we do a linear pass using the input array itself as a hash to store which numbers have been seen before. We do this by making elements at certain indexes negative. See the full explanation here
+
+http://www.geeksforgeeks.org/find-duplicates-in-on-time-and-constant-extra-space/
+
         :type nums: List[int]
         :rtype: List[int]
         """

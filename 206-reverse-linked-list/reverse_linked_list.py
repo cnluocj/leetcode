c126ffde13ebf0296a99ace1aa139979d923eb13
luocj <cnluocj@gmail.com>
2017-06-15 16:03:59 +0800

    finish - 206 单链表反转

---
+++ 206-reverse-linked-list/reverse_linked_list.py
@@ -0,0 +1,28 @@
+"""
+93.25%
+
+单链表反转
+http://blog.csdn.net/feliciafay/article/details/6841115
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
+        return prev
+
+

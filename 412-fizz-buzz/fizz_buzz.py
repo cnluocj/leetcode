79aa8fc1c365ba788a9211010b42691821407f4c
luocj <cnluocj@gmail.com>
2017-06-27 10:27:19 +0800

    finish - 412

---
+++ 412-fizz-buzz/fizz_buzz.py
@@ -0,0 +1,23 @@
+"""
+39.61%
+
+return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1, n+1)]
+"""
+
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        result = []
+        for i in range(1, n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                result.append('FizzBuzz')
+            elif i % 3 == 0:
+                result.append('Fizz')
+            elif i % 5 == 0:
+                result.append('Buzz')
+            else:
+                result.append(str(i))
+        return result

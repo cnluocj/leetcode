32d92ffe6b69b1266051ac0823ef4babf12958af
luocj <cnluocj@gmail.com>
2017-07-03 09:57:23 +0800

    finish - 50

---
+++ 50-pow/pow.py
@@ -0,0 +1,12 @@
+"""
+96.89%
+"""
+
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        return x**n
